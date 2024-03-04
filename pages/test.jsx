500e6055c926b4845014e499d7ed5deaa9832333
jhs129 <john.h.schneider@gmail.com>
2024-03-04 10:02:09 -0500

    working non cms wired version of TopNavBar

---
+++ pages/test.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import TopNavBar from "@/components/layout/TopNavBar";
+
+export default function Home() {
+  return (
+    <main>
+      <TopNavBar />
+
+    </main>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '780px',
+      lg: '1024px',
+    },
+    extend: {
+      colors: {
+        primaryLight: '#FFF',
+        secondaryLight: '#ededed',
+        primaryDark: '#393839',
+        secondaryDark: '#dfdfdf',
+        primaryAccent: '#CF4B08',
+        secondaryAccent: '#eee47b',
+        tertiaryAccent: 'hsl(192, 100%, 45%, .7)',
+      },
+      fontFamily: {
+        sans: ['Lufga', 'sans-serif'],
+        serif: ['Aspira', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+}
