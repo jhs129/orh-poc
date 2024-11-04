50b452e089f3dce5bd3ed3d90725ea33c1712982
jhs129 <john.h.schneider@gmail.com>
2024-11-04 15:09:41 -0500

    added locale cookie to app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,14 @@
 import "@/styles/globals.css";
 import '@/styles/fonts.css';
+import Cookies from 'js-cookie';
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    const locale = Cookies.get('locale') || 'en-US';
+    if (!Cookies.get('locale')) {
+      Cookies.set('locale', locale, { expires: 365 });
+    }
+  }, []);
   return <Component {...pageProps} />;
 }
